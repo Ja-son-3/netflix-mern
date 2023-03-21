8ed9d150651dd535e4a92ac5586aefc2ac02bd79
Jason Huang <jason97521@gmail.com>
2023-03-21 05:05:05 -0400

    node.js register

--- api/models/List.js
+++ api/models/List.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose")
 
 const ListSchema = new mongoose.Schema({
-    username:{
-        title:{type:String, required: true, unique: true},
-        type:{type:String},
-        genre:{type:String},
-        context:{type:Array}
-    }
+    title:{type:String, required: true, unique: true},
+    type:{type:String},
+    genre:{type:String},
+    context:{type:Array}
 },{timestamps:true})
 
-module.export = mongoose.model("List", ListSchema)
+module.exports = mongoose.model("List", ListSchema)
